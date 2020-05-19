b395f5b48242488ee6870e21ad5afc6fbe32e7f5
peterdao9 <65024146+peterdao9@users.noreply.github.com>
2020-05-19 21:59:28 +1000

    Add files via upload

---
+++ CovidCHAL/CovidCHAL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidCHAL
+//
+//  Created by Dang Dao on 19/5/20.
+//  Copyright © 2020 PeterDao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
