4b7d1d90c451832804f4d52b2538a4c37e04dcc0
peterdao9 <65024146+peterdao9@users.noreply.github.com>
2020-05-21 07:46:13 +1000

    Add files via upload

---
+++ CovidCHAL/CovidCHAL/HomeCollectionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeCollectionViewController.swift
+//  CovidCHAL
+//
+//  Created by Dang Dao on 21/5/20.
+//  Copyright © 2020 PeterDao. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class HomeCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+}
